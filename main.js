649c59b04b4bcf4a583a8c0c84bbc424e787bdd6
Wallrisss <Robbyrev7@gmail.com>
2025-09-08 18:42:12 +0000

    winning path done

--- main.js
+++ main.js
@@ -13,22 +13,29 @@ while (fail) {
   {
    let food = await ti.prompt("Survive the diet by answering trivia. what did my mother eat?");
    if (food.toLowerCase() === "fries") 
-{
-   let trewf = await ti.prompt("what did tanya eat?")}
-   if (trewf.toLowerCase()==="slices")
+   //{else}
+   //await ti.output("no")
+   //fail = true}
+   
+    {
+   let tfood = await ti.prompt("what did tanya eat?")
+   if (tfood.toLowerCase()==="slices")
+    
    {
-    ti.output("blah blah blah")
-     } else {
-      await ti.output("no")}
-
-
-
-
-  } else {
+    let dfood = await ti.prompt("what did drew eat")
+     if (dfood.toLocaleLowerCase()==="donuts")
+     {
+     let Ifood =await ti.prompt("what did I eat")
+      if (Ifood.toLocaleLowerCase()==="all")
+      ti.output("YOU WIN!!!!")
+     }else{
+      ti.output("You fail")
+      fail = true
+     }
+   }
+  }
+} else {
     await ti.output("type start dummy") 
     fail = true;
-    
-
-    
   }
 }
